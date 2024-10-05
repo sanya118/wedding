ec25cfa0d8f62ff516f17539f0f1c6fe94897f2c
Agampreet Singh <agampreet_singh@yahoo.com>
2024-10-06 00:36:10 +0530

    notifications and service worker fix

--- js/Orchestrator.js
+++ js/Orchestrator.js
@@ -40,7 +40,7 @@ function getFCMToken() {
 
 // Register service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('config/firebase-messaging-sw.js')
+  navigator.serviceWorker.register('https://artisticgeek.github.io/wedding/config/firebase-messaging-sw.js')
     .then((registration) => {
       console.log('Service Worker registered:', registration);
     }).catch((err) => {
